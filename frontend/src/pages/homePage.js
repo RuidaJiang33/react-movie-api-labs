7d8a80c3dabb12eb20e6a05362ff5903b3471a7c
RuidaJiang33 <772456648@qq.com>
2023-12-05 14:23:41 +0000

    change structure

---
+++ frontend/src/pages/homePage.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { getMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateMovieListPage';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToFavoritesIcon from '../components/cardIcons/addToFavorites'
+
+const HomePage = (props) => {
+
+  const { data, error, isLoading, isError } = useQuery('discover', getMovies)
+
+  if (isLoading) {
+    return <Spinner />
+  }
+
+  if (isError) {
+    return <h1>{error.message}</h1>
+  }
+  const movies = data.results;
+
+  return (
+    <PageTemplate
+      title="Discover Movies"
+      movies={movies}
+      action={(movie) => {
+        return <AddToFavoritesIcon movie={movie} />
+      }}
+    />
+  );
+};
+export default HomePage;
