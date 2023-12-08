3671653452865a63d5b68af09b51be16f413ae8a
RuidaJiang33 <772456648@qq.com>
2023-12-08 09:58:01 +0000

    modify backend

--- backend/api/movies/index.js
+++ backend/api/movies/index.js
@@ -2,8 +2,8 @@ import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
 import express from 'express';
 import {
-  getUpcoming,
-  getTopRated
+  getUpcomingMovies,
+  getGenres
 } from '../tmdb-api';
 
 const router = express.Router();
@@ -41,12 +41,12 @@ router.get('/:id', asyncHandler(async (req, res) => {
 }));
 
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
-  const upcomingMovies = await getUpcoming();
+  const upcomingMovies = await getUpcomingMovies();
   res.status(200).json(upcomingMovies);
 }));
 
 router.get('/tmdb/genres', asyncHandler(async (req, res) => {
-  const genres = await getTopRated();
+  const genres = await getGenres();
   res.status(200).json(genres);
 }));
 
